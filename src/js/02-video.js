62eccf39237a0053e778bd8c7701e94c800cd611
AlexWSN <sasualexandra97@gmail.com>
2024-11-06 19:00:14 +0200

    video player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector("iframe"); // Selectează iframe-ul din HTML
+const player = new Player(iframe); // Inițializează player-ul Vimeo
+
+player.on("timeupdate", function (event) {
+  const currentTime = event.seconds; // Timpul de redare curent al videoclipului
+  localStorage.setItem("videoplayer-current-time", currentTime); // Salvează timpul în localStorage
+});
+
+const savedTime = localStorage.getItem("videoplayer-current-time"); // Obține timpul salvat
+if (savedTime) {
+  player
+    .setCurrentTime(savedTime)
+    .then(function () {})
+    .catch(function (error) {
+      console.error("Eroare la setarea timpului:", error);
+    });
+}
+
+const throttledSaveTime = throttle(function (event) {
+  const currentTime = event.seconds;
+  localStorage.setItem("videoplayer-current-time", currentTime);
+}, 1000); // Limitează la un apel pe secundă
+
+player.on("timeupdate", throttledSaveTime);
