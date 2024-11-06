3d8aaf68fb95c070a0c1c4e05f264cbf9f2589bc
AlexWSN <sasualexandra97@gmail.com>
2024-11-06 20:44:58 +0200

    feedback form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from "lodash.throttle";
 
+// Selectează formularul și câmpurile din formular
+const form = document.querySelector(".feedback-form");
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+// Funcția de salvare a stării formularului în localStorage
+const saveFormState = throttle(function () {
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  // Salvează în localStorage
+  localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+}, 500);
+
+form.addEventListener("input", saveFormState);
+window.addEventListener("load", () => {
+  const savedData = JSON.parse(localStorage.getItem("feedback-form-state"));
+
+  if (savedData) {
+    emailInput.value = savedData.email || "";
+    messageInput.value = savedData.message || "";
+  }
+});
+
+// Gestionarea evenimentului de submit
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  console.log(formData);
+
+  // Șterge datele din localStorage după trimiterea formularului
+  localStorage.removeItem("feedback-form-state");
+
+  form.reset();
+});
